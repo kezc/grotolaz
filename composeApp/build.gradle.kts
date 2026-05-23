4d6da678fb390c712065d8c22a6dfd9c8e8a8c06
wojtek <krkezc@gmail.com>
2026-05-23 16:52:40 +0200

    Add web dependency for IDE resolution

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.kotlinx.browser)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.serialization.json)
